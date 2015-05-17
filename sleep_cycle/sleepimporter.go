33357f883f7c74f27e1d7c8bff5aaec9d7ce7024
pyohei <mukaishohei76@gmail.com>
2015-05-17 15:46:44 +0900

    dekita-----

--- sleep_cycle/sleepimporter.go
+++ sleep_cycle/sleepimporter.go
@@ -27,14 +27,19 @@ func main() {
 	//	lines := make([]string, 0)
 
 	//	my_lines := make([]string, 120)
+	bbb := make([][]string, 0, 200)
 	scanner := bufio.NewScanner(fp)
 	for i := 0; scanner.Scan(); i++ {
 		r := strings.NewReplacer("\n", "")
 		my_line := strings.Split(r.Replace(scanner.Text()), ";")
-		fmt.Println("----")
-		fmt.Println(my_line)
-		fmt.Println(len(my_line))
+		//aaa := make([]string, 0, 200)
+		//aaa = append(aaa, my_line[0])
+		//aaa = append(aaa, my_line[1])
+		bbb = append(bbb, my_line)
+		//fmt.Print(aaa)
+		//	fmt.Println(len(my_line))
 	}
+	fmt.Println(bbb)
 	if err := scanner.Err(); err != nil {
 		panic(err)
 	}
