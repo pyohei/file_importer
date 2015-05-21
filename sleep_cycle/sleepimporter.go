30889f7709554ca503fa05a9154255c585bff55a
pyohei <mukaishohei76@gmail.com>
2015-05-21 23:14:47 +0900

    clean source

--- sleep_cycle/sleepimporter.go
+++ sleep_cycle/sleepimporter.go
@@ -3,19 +3,22 @@ package main
 import (
 	"bufio"
 	"database/sql"
-	"fmt"
 	_ "github.com/go-sql-driver/mysql"
+	"log"
 	"os"
 	"strings"
 )
 
-func main() {
-	var fp *os.File
-	var err error
+const LOG_FILE = "sleepimporter.log"
 
-	fmt.Println("---- start -----")
+func main() {
+
+	wf, _ := os.OpenFile(LOG_FILE, os.O_WRONLY|os.O_CREATE, 0755)
+	log.SetOutput(wf)
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	log.Println("START!")
 	if len(os.Args) != 2 {
-		fmt.Println("You should hava argument with filename")
+		log.Println("You should hava argument with filename")
 		os.Exit(1)
 	}
 	//fmt.Printf(">> read file %s\n", os.Args[1])
@@ -24,8 +27,6 @@ func main() {
 	//fmt.Println(filelines)
 
 	/*
-		// 上記のelseをなくし、ここからファイルの読み込み->
-		// 一覧の取得->DBへの追加をするようにする
 		// それができたら、ひとまずこのbatchは終わりとする。
 		// 統計結果を出すのはまた別。
 		// SQLの見直しも必要
@@ -35,47 +36,15 @@ func main() {
 		if i == 0 {
 			continue
 		}
-		fmt.Println(i, line)
-	}
-	cnn, err := sql.Open(
-		"mysql",
-		"developer:developer@tcp(192.168.0.90:3306)/sleep_cycle?charset=utf8")
-	stmt, err := cnn.Prepare(
-		"INSERT sleep SET sleep_from=?, sleep_to=?, sleep_len=?")
-	res, err := stmt.Exec("time", "time", "time")
-	fmt.Println(res)
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer stmt.Close()
-	/*
-		for rows.Next() {
-			var user_name string
-			if err := rows.Scan(&user_name); err != nil {
-				fmt.Println(err)
-			}
-			fmt.Println(user_name)
+		err := insertRecord(line)
+		if err != nil {
+			log.Printf("Line: %v Error!!", i)
+			panic(err)
 		}
-		if err := rows.Err(); err != nil {
-			fmt.Println(err)
-		}
-	*/
-	bbb := make([][]string, 0, 200)
-	scanner := bufio.NewScanner(fp)
-	for i := 0; scanner.Scan(); i++ {
-		r := strings.NewReplacer("\n", "")
-		my_line := strings.Split(r.Replace(scanner.Text()), ";")
-		//aaa := make([]string, 0, 200)
-		//aaa = append(aaa, my_line[0])
-		//aaa = append(aaa, my_line[1])
-		bbb = append(bbb, my_line)
-		//fmt.Print(aaa)
-		//	fmt.Println(len(my_line))
+		log.Printf("Line: %v Succcess!!", i)
 	}
-	fmt.Println(bbb)
-	if err := scanner.Err(); err != nil {
-		panic(err)
-	}
+	log.Println("FINISH")
+	wf.Close()
 }
 
 func fileReader(filepath string) [][]string {
@@ -98,9 +67,29 @@ func fileReader(filepath string) [][]string {
 		//fmt.Print(aaa)
 		//	fmt.Println(len(my_line))
 	}
-	fmt.Println(bbb)
 	if err := scanner.Err(); err != nil {
 		panic(err)
 	}
 	return bbb
+}
+
+func insertRecord(rec []string) error {
+	conn, err := sql.Open(
+		"mysql",
+		"developer:developer@tcp(192.168.0.90:3306)/sleep_cycle?charset=utf8")
+	if err != nil {
+		return err
+	}
+	stmt, err := conn.Prepare(
+		"INSERT sleep SET sleep_from=?, sleep_to=?, sleep_len=?")
+	if err != nil {
+		return err
+	}
+	res, err := stmt.Exec(rec[0], rec[1], rec[3])
+	_ = res
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	return nil
 }
